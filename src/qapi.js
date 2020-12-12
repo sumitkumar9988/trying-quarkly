fc75bdf776385305990054b0f75cb03f44f78b87
sumit Kumar <55837044+sumitkumar9988@users.noreply.github.com>
2020-12-12 23:04:54 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd455056d47ab001e03000a",
+				"5fd455056d47ab001e03000c"
+			]
+		},
+		"5fd455056d47ab001e03000a": {
+			"id": "5fd455056d47ab001e03000a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd455056d47ab001e03000c": {
+			"id": "5fd455056d47ab001e03000c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
